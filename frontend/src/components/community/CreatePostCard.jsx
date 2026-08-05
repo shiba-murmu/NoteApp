f25149ef58389ad010b1e683208c1e55f7e79cdc
Shiba Murmu <shibamurmu001@gmail.com>
2026-08-05 20:37:50 +0530

    deleting bymistakes code written , broken consistens

--- frontend/src/components/community/CreatePostCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function CreatePostCard() {
-  return (
-    <div className="rounded-3xl border border-white/10 bg-[#111827] p-8">
-      Create Post Card
-    </div>
-  );
-}
--- frontend/src/components/community/Feed.jsx
+++
@@ -1,7 +0,0 @@
-export default function Feed() {
-  return (
-    <div className="rounded-3xl border border-white/10 bg-[#111827] p-8">
-      Feed Component
-    </div>
-  );
-}
