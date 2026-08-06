0fcd58cf770e38d108fccaf24ee1007be0ae06aa
Shiba Murmu <shibamurmu001@gmail.com>
2026-08-06 07:43:53 +0530

    conditional rendering feature

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import Navbar from './components/layout/Navbar.jsx'
 import Footer from './components/layout/Footer.jsx'
 
+
+function Layout() {
+    const location = useLocation();
+
+    // Routes that should NOT show the landing navbar/footer
+    const hideLayout = location.pathname.startsWith("/dashboard");
+    return (
+        <>
+            {!hideLayout && <Navbar />}
+            <App />
+            {!hideLayout && <Footer />}
+        </>
+    );
+}
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <Navbar />
-                <App />
-            <Footer />
+            <Layout />
         </BrowserRouter>
     </StrictMode>,
 )
