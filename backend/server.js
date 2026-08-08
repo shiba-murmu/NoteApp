7300ceb2b079fb3d5a56efff18970c864e21974b
Shiba Murmu <shibamurmu001@gmail.com>
2026-08-08 20:28:10 +0530

    backend is installed and added to the project .

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message : "Nexora backend is running 🚀",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
